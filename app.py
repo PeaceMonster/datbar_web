5a4f39862eec3999dee078400819e601ee93a28b
Ugilt <ugilt3@gmail.com>
2024-01-08 17:56:48 +0100

    Start on form

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 
 app = Flask(__name__)
 
-@app.route("/")
+
+
+@app.route("/", methods=["POST", "GET"])
 def std_path():
+    if request.method == "POST":
+        print(request.form)
     return send_from_directory('frontend/dist/', 'index.html')
+
 
 @app.route("/<path:path>")
 def alternate_path(path):
@@ -21,3 +26,5 @@ def admin_pth():
 def serve_static(path):
     return send_from_directory('frontend/dist/assets', path)
 
+if __name__ == '__main__':
+    app.run()
